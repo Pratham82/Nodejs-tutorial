1eb6db0e933da76672293bddbd15172f5f9b7f4f
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-25 19:02:17 +0530

    Completed CRUD operation and tested the operations

--- Node.js Masterclass No frameworks and npm/Section 3/Backend app/index.js
+++ Node.js Masterclass No frameworks and npm/Section 3/Backend app/index.js
@@ -1,7 +1,6 @@
 /*
 Primary file for the API
 
-
 */
 
 // Starting a server
@@ -12,8 +11,43 @@ var url = require("url");
 var StringDecoder = require("string_decoder").StringDecoder;
 var config = require("./config");
 var fs = require("fs");
+var _data = require("./lib/data");
 
 // console.log(http);
+
+//* Testing the library that we have created
+// TODO CREATE new file and write operation
+// _data.create("test", "testingFile", { name: "Jason" }, function (err) {
+// 	console.log(
+// 		err == false ? "Data successfully written to the file ✅" : "Error 😢 ",
+// 		err
+// 		// "This was error",
+// 		// err
+// 	);
+// });
+
+// // TODO Testing read file operation
+// _data.read("test", "testingFile", function (err, data) {
+// 	console.log(
+// 		// err == false ? `Data from the file ${data}` : "Error 😢 ",
+// 		// err
+// 		!err ? `Data from the file ${data}` : `Error 😢 \n${err}`
+// 	);
+// });
+
+// // TODO Testing update file operation
+// _data.update("test", "testingFile", { name: "New data" }, function (err) {
+// 	console.log(
+// 		err == false ? "Data successfully updated ✅" : ` Error 😢  ${err}`
+// 	);
+// });
+
+// TODO Testing delete file operation
+_data.delete("test", "testingFile", function (err) {
+	console.log(
+		err == false ? "File successfully deleted ✅" : ` Error 😢  ${err}`
+	);
+});
 
 //* Instantiating the HTTP server
 var HTTPServer = http.createServer(function (req, res) {
