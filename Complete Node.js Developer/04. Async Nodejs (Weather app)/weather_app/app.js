de209aa96e9b2b123eaebd6b9cd2c670057f6140
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-04 17:26:34 +0530

    Added geolocation API

--- Complete Node.js Developer/04. Async Nodejs (Weather app)/weather_app/app.js
+++ Complete Node.js Developer/04. Async Nodejs (Weather app)/weather_app/app.js
@@ -1,11 +1,35 @@
 const request = require("request");
 const fetch = require("node-fetch");
-const url =
-	"https://api.openweathermap.org/data/2.5/weather?q=new%20york&units=metric&APPID=b0395dee1e7a64a9f7ef16800ef19dab";
+require("dotenv").config();
+const url = `https://api.openweathermap.org/data/2.5/weather?q=new%20york&units=metric&APPID=${process.env.API_KEY}`;
 
-request({ url, json: true }, (error, response) => {
-	const data = response.body;
-	// console.log(data);s
-	console.log(`Today's forecast: ${data.weather[0].description}`);
-	console.log(`It's currently ${24.11}°C `);
+request({ url, json: true }, (err, response) => {
+	if (err) {
+		console.log(
+			`Unable to connect to the weather service provided by ${err.hostname}`
+		);
+	} else if (response.body.err) {
+		console.log(`Unable to find location`);
+	} else {
+		console.log(
+			`Today's forecast: ${response.body.weather[0].description}`
+		);
+		console.log(`It's currently ${response.body.main.temp}°C `);
+	}
+});
+
+//* Geocoding service
+const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/new%20york.json?access_token=${process.env.ACCESS_TOKEN}`;
+
+request({ url: geoUrl, json: true }, (err, response) => {
+	if (err) {
+		console.log(
+			`Cannot connect to location service provided by ${err.hostname}`
+		);
+	} else if (response.body.features.length == 0) {
+		console.log("Unable to find location");
+	} else {
+		console.log(`Latitude: ${response.body.features[3].center[0]}`);
+		console.log(`Longitude: ${response.body.features[3].center[1]}`);
+	}
 });
