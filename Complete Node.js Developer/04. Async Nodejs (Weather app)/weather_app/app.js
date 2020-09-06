74fa4271272527006c511afcdc9825b8229a5a15
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-06 23:21:07 +0530

    Commented out previous requests and added geoCode

--- Complete Node.js Developer/04. Async Nodejs (Weather app)/weather_app/app.js
+++ Complete Node.js Developer/04. Async Nodejs (Weather app)/weather_app/app.js
@@ -1,35 +1,64 @@
 const request = require("request");
 const fetch = require("node-fetch");
-require("dotenv").config();
-const url = `https://api.openweathermap.org/data/2.5/weather?q=new%20york&units=metric&APPID=${process.env.API_KEY}`;
+const geoCode = require("./utils");
 
-request({ url, json: true }, (err, response) => {
-	if (err) {
-		console.log(
-			`Unable to connect to the weather service provided by ${err.hostname}`
-		);
-	} else if (response.body.err) {
-		console.log(`Unable to find location`);
-	} else {
-		console.log(
-			`Today's forecast: ${response.body.weather[0].description}`
-		);
-		console.log(`It's currently ${response.body.main.temp}°C `);
-	}
-});
+require("dotenv").config();
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=new%20york&units=metric&APPID=${process.env.API_KEY}`;
+
+// request({ url, json: true }, (err, response) => {
+// 	if (err) {
+// 		console.log(
+// 			`Unable to connect to the weather service provided by ${err.hostname}`
+// 		);
+// 	} else if (response.body.err) {
+// 		console.log(`Unable to find location`);
+// 	} else {
+// 		console.log(
+// 			`Today's forecast: ${response.body.weather[0].description}`
+// 		);
+// 		console.log(`It's currently ${response.body.main.temp}°C `);
+// 	}
+// });
 
 //* Geocoding service
 const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/new%20york.json?access_token=${process.env.ACCESS_TOKEN}`;
 
-request({ url: geoUrl, json: true }, (err, response) => {
-	if (err) {
-		console.log(
-			`Cannot connect to location service provided by ${err.hostname}`
-		);
-	} else if (response.body.features.length == 0) {
-		console.log("Unable to find location");
-	} else {
-		console.log(`Latitude: ${response.body.features[3].center[0]}`);
-		console.log(`Longitude: ${response.body.features[3].center[1]}`);
-	}
+// request({ url: geoUrl, json: true }, (err, response) => {
+// 	if (err) {
+// 		console.log(
+// 			`Cannot connect to location service provided by ${err.hostname}`
+// 		);
+// 	} else if (response.body.features.length == 0) {
+// 		console.log("Unable to find location");
+// 	} else {
+// 		console.log(`Latitude: ${response.body.features[3].center[0]}`);
+// 		console.log(`Longitude: ${response.body.features[3].center[1]}`);
+// 	}
+// });
+
+//* One call API
+// const fs = require("fs");
+const openAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=19.140512&lon=72.842155&units=metric&appid=${process.env.API_KEY}`;
+
+// fetch(openAPI)
+// 	.then((res) => res.json())
+// 	.then((data) =>
+// 		console.log(
+// 			`Its ${data.current.temp} °C, feels like ${data.current.feels_like} in \nToday's forecast: ${data.current.weather[0].main}`
+// 		)
+// 	)
+// 	.catch((err) => console.log(err));
+
+// `Its ${data.current.temp} °C, feels like  ${data.current.feels_like} in \nToday's forecast: ${data.current.weather[0].main}`;
+
+geoCode("Mumbai", (error, response) => {
+	console.log({
+		Response: response,
+		Error: error,
+	});
+	return {
+		Response: response,
+		Error: error,
+	};
 });
