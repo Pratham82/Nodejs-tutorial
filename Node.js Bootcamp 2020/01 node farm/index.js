176d8d8da25dcd27902827f997b441e290512dc4
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-12-01 17:43:41 +0530

    Created and added replaceTemplate modulec

--- Node.js Bootcamp 2020/01 node farm/index.js
+++ Node.js Bootcamp 2020/01 node farm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate')
 
 ///////////////////////////////// File Handling ///////////////////////////////
 // Blocking, synchronous way
@@ -39,20 +40,6 @@ console.log('synchronous code ')
 ///////////////////// Creating simple HTTP server /////////////////////////////
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%NUTRIENTSNAME%}/g, product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%ID%}/g, product.id)
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-  return output
-}
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
